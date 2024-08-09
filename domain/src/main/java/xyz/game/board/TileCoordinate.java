2b2d721d9972be3185b3e196094bb77222ef4493
JanPfenning <jan.pfenning@gmail.com>
2024-08-09 14:50:49 +0200

    fix stack overflow on constructor of TileCoordinate

--- domain/src/main/java/xyz/game/board/TileCoordinate.java
+++ domain/src/main/java/xyz/game/board/TileCoordinate.java
@@ -1,7 +1,6 @@
 package xyz.game.board;
 
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class TileCoordinate {
@@ -12,18 +11,13 @@ public class TileCoordinate {
     @EqualsAndHashCode.Include
     private final int y;
 
-    @Getter
-    private final TileCoordinate[] surroundingTileCoordinates;
-    @Getter
-    private final EdgeCoordinate[] surroundingEdgeCoordindates;
-    @Getter
-    private final VertexCoordinate[] surroundingVertexCoordindates;
-    
     public TileCoordinate(int x, int y) {
         this.x = x;
         this.y = y;
+    }
 
-        this.surroundingTileCoordinates = new TileCoordinate[]{
+    public TileCoordinate[] getSurroundingTileCoordinates() {
+        return new TileCoordinate[]{
             new TileCoordinate(x + 0, y - 1), // N
             new TileCoordinate(x + 1, y + (x % 2 == 0 ? -1 : 0)), // NE
             new TileCoordinate(x + 1, y + (x % 2 == 0 ? 0 : +1)), // SE
@@ -31,11 +25,13 @@ public class TileCoordinate {
             new TileCoordinate(x - 1, y + (x % 2 == 0 ? 0 : +1)), // SW
             new TileCoordinate(x - 1, y + (x % 2 == 0 ? -1 : 0)), // NW
         };
+    };
 
+    public EdgeCoordinate[] getSurroundingEdgeCoordindates() {
         int baseEdgeX = x * 2;
         int baseEdgeOfsetY = x % 2 == 1 ? 2 : 0;
         int baseEdgeY = y * 4 + baseEdgeOfsetY;
-        this.surroundingEdgeCoordindates = new EdgeCoordinate[]{
+        return new EdgeCoordinate[]{
             new EdgeCoordinate(baseEdgeX+1, baseEdgeY+0), // N
             new EdgeCoordinate(baseEdgeX+2, baseEdgeY+1), // NE
             new EdgeCoordinate(baseEdgeX+2, baseEdgeY+3), // SE
@@ -44,9 +40,12 @@ public class TileCoordinate {
             new EdgeCoordinate(baseEdgeX+0, baseEdgeY+1),  // NW
         };
 
+    };
+
+    public VertexCoordinate[] getSurroundingVertexCoordindates() {
         int baseVertexOfsetY = x % 2 == 0 ? 0 : 1;
         int baseVertexY = y * 2 + baseVertexOfsetY;
-        this.surroundingVertexCoordindates = new VertexCoordinate[]{
+        return new VertexCoordinate[]{
             new VertexCoordinate(x+1, baseVertexY+0), // NE
             new VertexCoordinate(x+1, baseVertexY+1), // E
             new VertexCoordinate(x+1, baseVertexY+2), // SE
@@ -54,11 +53,10 @@ public class TileCoordinate {
             new VertexCoordinate(x+0, baseVertexY+1), // W
             new VertexCoordinate(x+0, baseVertexY+0),  // NW
         };
-    }
+    };
 
     @Override
     public String toString() {
         return "T(" + x + "," + y + ")";
     }
-
 }
