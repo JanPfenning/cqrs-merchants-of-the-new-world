bf227cafc842ab710aa53c27e6ea944834311ba3
JanPfenning <jan.pfenning@gmail.com>
2024-08-10 22:32:47 +0200

    change VertexCoordinate from record to value object

--- domain/src/main/java/xyz/game/board/VertexCoordinate.java
+++ domain/src/main/java/xyz/game/board/VertexCoordinate.java
@@ -1,6 +1,21 @@
 package xyz.game.board;
 
-public record VertexCoordinate(int x, int y){
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class VertexCoordinate{
+
+    @EqualsAndHashCode.Include
+    private final int x;
+
+    @EqualsAndHashCode.Include
+    private final int y;
+
+    public VertexCoordinate(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
     @Override
     public String toString() {
         return "V(" + x + "," + y + ")";
