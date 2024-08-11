12ed8bac76aa8b9d91f9d73461c13fcbbd746d31
JanPfenning <jan.pfenning@gmail.com>
2024-08-11 14:48:36 +0200

    use board from domain

--- api/src/main/java/xyz/game/board/BoardController.java
+++ api/src/main/java/xyz/game/board/BoardController.java
@@ -3,10 +3,18 @@ package xyz.game.board;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import xyz.game.board.Board;
+
 @RestController
 public class BoardController {
     @GetMapping("/ping")
     public String sayHello() {
         return "pong!";
+    }
+
+    @GetMapping("/board")
+    public Board returnBoard() {
+        GridBoard b = new GridBoard(10, 5);
+        return b;
     }
 }
