b5d6ae8a85ecfa10d21e7ced89607df28dd5251b
JanPfenning <jan.pfenning@gmail.com>
2024-08-11 14:02:00 +0200

    fix project structure

--- api/src/test/java/xyz/game/merchants/MerchantsApplicationTests.java
+++ api/src/test/java/xyz/game/merchants/MerchantsApplicationTests.java
@@ -2,9 +2,7 @@ package xyz.game.merchants;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class MerchantsApplicationTests {
 
