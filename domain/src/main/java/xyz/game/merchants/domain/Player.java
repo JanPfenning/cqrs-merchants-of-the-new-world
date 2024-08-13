9a04cf0f92cf6853d23e45576c4041ab0e2d2b45
JanPfenning <jan.pfenning@gmail.com>
2024-08-13 23:44:26 +0200

    change deterministic UUID to rand UUID

--- domain/src/main/java/xyz/game/merchants/domain/Player.java
+++ domain/src/main/java/xyz/game/merchants/domain/Player.java
@@ -2,11 +2,15 @@ package xyz.game.merchants.domain;
 
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Player {
-    UUID uuid = new UUID(0, 0);
+    public final UUID uuid;
     public final String name;
     
     public Player(String name) {
         this.name = name;
+        this.uuid = UUID.randomUUID();
     }
 }
