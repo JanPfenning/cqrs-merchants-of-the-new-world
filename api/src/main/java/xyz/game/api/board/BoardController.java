af5a3453a4cf5a45f799a389a6984a9dbff82b3a
JanPfenning <jan.pfenning@gmail.com>
2024-08-13 08:50:46 +0200

    introduce application module and fix poms
    resolves issue #1 by changing the configuration of spring-boot-maven-plugin skip attribute to true in the parent/root pom

--- api/src/main/java/xyz/game/api/board/BoardController.java
+++ api/src/main/java/xyz/game/api/board/BoardController.java
@@ -1,4 +1,4 @@
-package xyz.game.board;
+package xyz.game.api.board;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- application/src/test/java/xyz/game/merchants/MerchantsApplicationTests.java
+++ application/src/test/java/xyz/game/merchants/MerchantsApplicationTests.java
@@ -1,4 +1,4 @@
-package xyz.game;
+package xyz.game.merchants;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
