5fad49ad0afa5e9759b3ffba8bd1c84e2eae1ae9
JanPfenning <jan.pfenning@gmail.com>
2024-08-14 23:52:48 +0200

    add building member var and setter to Edge and Vertex

--- domain/src/main/java/xyz/game/merchants/domain/board/Edge.java
+++ domain/src/main/java/xyz/game/merchants/domain/board/Edge.java
@@ -1,13 +1,28 @@
 package xyz.game.merchants.domain.board;
 
+import org.springframework.lang.Nullable;
+
 import lombok.Getter;
 import lombok.NonNull;
+import xyz.game.merchants.domain.building.EdgeBuilding;
 
 public class Edge {
+
+    @Nullable
+    @Getter
+    private EdgeBuilding building;
+
     @NonNull
     @Getter
     private final EdgeCoordinate coordinate;
     Edge(EdgeCoordinate c){
         this.coordinate = c;
+    }
+
+    public void setBuilding(EdgeBuilding building) {
+        if(this.building != null) {
+            throw new RuntimeException("illegal state");
+        }
+        this.building = building;
     }
 }
--- domain/src/main/java/xyz/game/merchants/domain/board/Vertex.java
+++ domain/src/main/java/xyz/game/merchants/domain/board/Vertex.java
@@ -1,13 +1,38 @@
 package xyz.game.merchants.domain.board;
 
+import org.springframework.lang.Nullable;
+
 import lombok.Getter;
 import lombok.NonNull;
+import xyz.game.merchants.domain.building.City;
+import xyz.game.merchants.domain.building.Settlement;
+import xyz.game.merchants.domain.building.VertexBuilding;
 
 public class Vertex {
+    
+    @Nullable
+    @Getter
+    private VertexBuilding building;
+
     @NonNull
     @Getter
     private final VertexCoordinate coordinate;
     Vertex(VertexCoordinate v) {
         this.coordinate = v;
+    }
+
+    public void setBuilding(Settlement settlement) {
+        if(this.building != null) {
+            throw new RuntimeException("illegal state");
+        }
+        this.building = settlement;
+    }
+
+    @SuppressWarnings("null")
+    public void setBuilding(City city) {
+        if(this.building == null || !(this.building instanceof Settlement) || this.building.getOwner() != city.getOwner()) {
+            throw new RuntimeException("illegal state");
+        }
+        this.building = city;
     }
 }
